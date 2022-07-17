ea5c8d66286138fe731701dd02fcb5d3a59a3bb6
Tu61 <tunguyen6131@gmail.com>
2022-07-17 19:11:36 +0700

    register

--- src/java/entity/Account.java
+++ src/java/entity/Account.java
@@ -7,111 +7,19 @@ import java.util.Date;
 
 public class Account {
     private int id;
-    private String name, pass;
-    private String role;
-    private int active;
-    private String pName,address,phone,gender;
-    private Date year;
+    private String userName, pass;
+    private String role ;
+    private int isActive;
+   
     public Account() {
     }
 
-    public Account(int id, String name, String pass, String role) {
+    public Account(int id, String userName, String pass, String role, int isActive) {
         this.id = id;
-        this.name = name;
+        this.userName = userName;
         this.pass = pass;
         this.role = role;
-    }
-
-    public Account(int id, String name, String pass, String pName, String address,
-            String phone, String gender, Date year) {
-        this.id = id;
-        this.name = name;
-        this.pass = pass;
-        this.pName = pName;
-        this.address = address;
-        this.phone = phone;
-        this.gender = gender;
-        this.year = year;
-    }
-
-    public Account(int id, String name, String pass, String role, 
-            int active, String pName, String address, String phone,
-            String gender, Date year) {
-        this.id = id;
-        this.name = name;
-        this.pass = pass;
-        this.role = role;
-        this.active = active;
-        this.pName = pName;
-        this.address = address;
-        this.phone = phone;
-        this.gender = gender;
-        this.year = year;
-    }
-    
-    public int getActive() {
-        return active;
-    }
-
-    public void setActive(int active) {
-        this.active = active;
-    }
-
-    public String getpName() {
-        return pName;
-    }
-
-    public void setpName(String pName) {
-        this.pName = pName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public Date getYear() {
-        return year;
-    }
-
-    public void setYear(Date year) {
-        this.year = year;
-    }
-
-    
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPass() {
-        return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
+        this.isActive = isActive;
     }
 
     public int getId() {
@@ -122,6 +30,22 @@ public class Account {
         this.id = id;
     }
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
     public String getRole() {
         return role;
     }
@@ -130,11 +54,19 @@ public class Account {
         this.role = role;
     }
 
-    
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
 
     @Override
     public String toString() {
-        return id+""+name + " " + pass +" "+pName+" "+address+" "+gender+" "+phone+" "+year;
+        return "Account{" + "id=" + id + ", userName=" + userName + ", pass=" + pass + ", role=" + role + ", isActive=" + isActive + '}';
     }
+
+   
 
 }
